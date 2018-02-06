83a6ff35dc2b4b8e49e8890ff378b457fd3898d9
Richard j Clarke <SilkyClarke+richardclarke1976@gmail.com>
2018-02-06 20:47:26 +0000

    rewrite of socket

--- server.js
+++ server.js
@@ -1,17 +1,77 @@
 'use strict';
 
+var bodyParser = require('body-parser');
+
 const express = require('express');
-const socketIO = require('socket.io');
+//const socketIO = require('socket.io');
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const app = express()
+    .use((req, res) => res.sendFile(INDEX) )
+    .use(express.static(__dirname))
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({extended:false}))
+    .use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
+        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS")});
+    //.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const io = socketIO(server);
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+
+
+mongoose.Promise = Promise;// used to override mongoose version of promise with updated es6 version
+
+var dbUrl = 'mongodb://richard:c0nvertib1e@ds225308.mlab.com:25308/waterlevel'
+
+var Message = mongoose.model('Message', {
+    level: String,
+    date:String
+})
+
+mongoose.connect(dbUrl, (err) => {
+    console.log('mongo db connection', err)
+})
+
+app.get('/messages', (req,res) =>{
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    }) 
+})
+
+
+app.post('/messages', async (req,res) =>{
+    
+    console.log("request is");
+    console.log(req.body);
+    
+    try {
+        var message = new Message(req.body)
+        console.log(message.level);
+        if (!message.level){
+            return console.error("no level value");
+        }         
+        //var savedMessage = await message.save()
+
+        console.log("saved")
+
+        io.emit('message', req.body)
+
+        res.sendStatus(200); // 200 is ok
+
+    } catch (error) {
+        res.sendStatus(500);
+        return console.error(error)
+    }
+
+})
+
 
 io.on('connection', (socket) => {
   console.log('Client connected');
@@ -19,3 +79,9 @@ io.on('connection', (socket) => {
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+
+var server = http.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+//var server = http.listen(3000, () => {
+//    console.log('server is listeneing on port', server.address().port)
+//})
